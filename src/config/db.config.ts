1138c5f71eaffc7f29c1212252c438566276b003
JoshMith <JoshMith@users.noreply.github.com>
2026-02-17 17:29:03 +0300

    ssl

--- src/config/db.config.ts
+++ src/config/db.config.ts
@@ -1,26 +1,30 @@
-import { Pool } from 'pg'
-import dotenv from 'dotenv'
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 
-    // For local development, comment out the ssl option
-    ssl: {
-        rejectUnauthorized: false, // Required for Render
-    },
-})
-pool.query('SELECT NOW()', (err, result) => {
+  // For local development, comment out the ssl option
+  // check if environment variable is set to production, if so, use ssl, otherwise do not use ssl
+  ssl:
+    process.env.ENVIRONMENT === "production"
+      ? {
+          rejectUnauthorized: false, // Required for Render
+        }
+      : false,
+});
+pool.query("SELECT NOW()", (err, result) => {
   if (err) {
-    console.error('Error connecting to database:', err);
+    console.error("Error connecting to database:", err);
   } else {
-    console.log('Database connected:', result.rows);
+    console.log("Database connected:", result.rows);
   }
 });
 
-export default pool
+export default pool;
